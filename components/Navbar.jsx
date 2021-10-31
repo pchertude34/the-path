5816ee1da0b88e5d4260e4dad62dc5e913c601af
Payton Chertude <p.chertude34@gmail.com>
2021-10-31 09:51:15 -0700

    try new percentages; update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -61,14 +61,7 @@ export default function WithSubnavigation() {
         </Flex>
 
         <Stack flex={{ base: 1, md: 0 }} justify={'flex-end'} direction="row" spacing={6}>
-          <Button
-            as="a"
-            fontSize="sm"
-            fontWeight={400}
-            variant="link"
-            href="#"
-            colorScheme="primary"
-          >
+          <Button as="a" href="#" colorScheme="primary">
             Find Your Path
           </Button>
         </Stack>
