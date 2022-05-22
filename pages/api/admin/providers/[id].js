9d94fe4e01d8eb7496f40bebd0af9dcd0bc256bc
Payton Chertude <p.chertude34@gmail.com>
2022-05-22 11:39:31 -0700

    add update functionality to provider service list

--- pages/api/admin/providers/[id].js
+++ pages/api/admin/providers/[id].js
@@ -20,32 +20,66 @@ const handler = nc({ onError })
   .put(async function updateProviderById(req, res) {
     const { id } = req.query;
     const { body } = req;
+    const updatedServices = body.serviceTypes;
 
     const providerData = await providerUpdateSchema.validate(body, {
       abortEarly: true,
       stripUnknown: true,
     });
 
-    const updatedProvider = await prisma.provider.update({
+    // Query the current provider data from the database so we can figure out what is changing.
+    const currentProvider = await prisma.provider.findUnique({
       where: { id: parseInt(id, 10) },
-      data: {
-        description: providerData.description,
+      include: {
+        serviceOnProvider: {
+          select: {
+            serviceId: true,
+          },
+        },
       },
     });
 
-    providerData.serviceTypes.forEach(async (serviceType) => {
-      try {
-        const serviceJoinQuery = Prisma.sql`
-          INSERT INTO \`the-path\`.ServiceOnProvider
-          VALUES (${id}, ${serviceType})
-        `;
-        await prisma.$executeRaw(serviceJoinQuery);
-      } catch (error) {
-        console.error(error);
-      }
-    });
+    // Get the current services associated with the provider so we can determin which ones to add
+    // and which ones to remove.
+    const currentServices = currentProvider.serviceOnProvider.map(
+      (serviceType) => serviceType.serviceId
+    );
 
-    res.status(200).send();
+    // Get a list of all services that aren't currently associated with the provider. We will add these to the provider later.
+    let servicesToAdd = updatedServices.filter((s) => !currentServices.includes(s));
+    // Get a list of all services that are currently associated with the provider, but aren't in the new service data.
+    // We will remove these services from the provider later.
+    let servicesToRemove = currentServices.filter((s) => !updatedServices.includes(s));
+
+    const updateProviderQuery = {
+      where: { id: parseInt(id, 10) },
+      data: {
+        description: providerData.description,
+        serviceOnProvider: {},
+      },
+    };
+
+    // Remove old services from the provider
+    if (servicesToRemove.length > 0) {
+      updateProviderQuery.data.serviceOnProvider.deleteMany = servicesToRemove.map((serviceId) => ({
+        serviceId,
+      }));
+    }
+
+    // Add new services to the provider
+    if (servicesToAdd.length > 0) {
+      updateProviderQuery.data.serviceOnProvider.create = servicesToAdd.map((serviceId) => ({
+        service: {
+          connect: {
+            id: serviceId,
+          },
+        },
+      }));
+    }
+
+    const updatedProvider = await prisma.provider.update(updateProviderQuery);
+
+    res.status(200).json(updatedProvider);
   })
   .delete(async function deleteProviderById(req, res) {
     const { id } = req.query;
