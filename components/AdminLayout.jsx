fa4cb9eddb6d3a97c554450484915efab7d68c77
Payton Chertude <p.chertude34@gmail.com>
2022-06-05 16:13:19 -0700

    position footer at bottom of screen

--- components/AdminLayout.jsx
+++ components/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import { Container } from '@chakra-ui/react';
+import { Container, Flex } from '@chakra-ui/react';
 import AdminNavbar from './AdminNavbar';
 import Footer from './Footer';
 
@@ -11,9 +11,13 @@ function AdminLayout({ children }) {
         <title>The Path</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <AdminNavbar />
-      <Container maxW="7xl">{children}</Container>
-      <Footer mt={4} />
+      <Flex direction="column">
+        <AdminNavbar />
+        <Container maxW="7xl" mb={8}>
+          {children}
+        </Container>
+        <Footer mt="auto" />
+      </Flex>
     </React.Fragment>
   );
 }
